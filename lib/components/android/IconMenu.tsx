dcb11c45cf7dc39fd7f92df144eb78abcead45d2
Dheeraj Kumar <dkchalotra@gmail.com>
2025-05-22 15:19:30 +0530

    prompt svg icons color set to currentColor

--- lib/components/android/IconMenu.tsx
+++ lib/components/android/IconMenu.tsx
@@ -5,7 +5,7 @@ export function IconMenu() {
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M14 6a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm0 6a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm-2 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
-        fill="context-fill"
+        fill="currentColor"
         fill-opacity=".8"
       ></path>
     </svg>
